3d0637e48e7cd1294f0db4221d68a105d2358fc6
Maxime Burlat <maximeburlat@gmail.com>
2018-05-15 08:28:33 -0500

    initial render works on route / remains to implement the routes for the burger controls and the Jquery code to request on those routes

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,4 +1,23 @@
 let express = require("express");
 let burger = require("./../models/burger");
 
-module.exports = app => {};
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  burger.fetchAllPromise().then(data => {
+    let dataObj = {
+      burgers: data
+    };
+    res.render("index", dataObj);
+  });
+});
+
+router.get("/api/burgers", (req, res) => {});
+
+router.post("/api/burgers", (req, res) => {});
+
+router.put("/api/burgers/:id", (req, res) => {});
+
+router.delete("/api/burgers/:id", (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,79 +18,73 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Use Handlebars to render the main index.html page with the todos in it.
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM plans;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controllers");
 
-    res.render("index", { plans: data });
-  });
-});
+app.use(routes);
 
 // Create a new todo
-app.post("/todos", function(req, res) {
-  connection.query(
-    "INSERT INTO plans (plan) VALUES (?)",
-    [req.body.plan],
-    function(err, result) {
-      if (err) {
-        return res.status(500).end();
-      }
+// app.post("/todos", function(req, res) {
+//   connection.query(
+//     "INSERT INTO plans (plan) VALUES (?)",
+//     [req.body.plan],
+//     function(err, result) {
+//       if (err) {
+//         return res.status(500).end();
+//       }
 
-      // Send back the ID of the new todo
-      res.json({ id: result.insertId });
-      console.log({ id: result.insertId });
-    }
-  );
-});
+//       // Send back the ID of the new todo
+//       res.json({ id: result.insertId });
+//       console.log({ id: result.insertId });
+//     }
+//   );
+// });
 
 // Retrieve all todos
-app.get("/todos", function(req, res) {
-  connection.query("SELECT * FROM plans;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
+// app.get("/todos", function(req, res) {
+//   connection.query("SELECT * FROM plans;", function(err, data) {
+//     if (err) {
+//       return res.status(500).end();
+//     }
 
-    res.json(data);
-  });
-});
+//     res.json(data);
+//   });
+// });
 
 // Update a todo
-app.put("/todos/:id", function(req, res) {
-  connection.query(
-    "UPDATE plans SET plan = ? WHERE id = ?",
-    [req.body.plan, req.params.id],
-    function(err, result) {
-      if (err) {
-        // If an error occurred, send a generic server failure
-        return res.status(500).end();
-      } else if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-    }
-  );
-});
+// app.put("/todos/:id", function(req, res) {
+//   connection.query(
+//     "UPDATE plans SET plan = ? WHERE id = ?",
+//     [req.body.plan, req.params.id],
+//     function(err, result) {
+//       if (err) {
+//         // If an error occurred, send a generic server failure
+//         return res.status(500).end();
+//       } else if (result.changedRows === 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
+//     }
+//   );
+// });
 
 // Delete a todo
-app.delete("/todos/:id", function(req, res) {
-  connection.query("DELETE FROM plans WHERE id = ?", [req.params.id], function(
-    err,
-    result
-  ) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    } else if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-  });
-});
+// app.delete("/todos/:id", function(req, res) {
+//   connection.query("DELETE FROM plans WHERE id = ?", [req.params.id], function(
+//     err,
+//     result
+//   ) {
+//     if (err) {
+//       // If an error occurred, send a generic server failure
+//       return res.status(500).end();
+//     } else if (result.affectedRows === 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     }
+//     res.status(200).end();
+//   });
+// });
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
